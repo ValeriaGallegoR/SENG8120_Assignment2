9076b0357370fcdcdb826ad94507e7e1070df8b9
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-12 15:01:19 -0400

    /paintRequiredCalculator.ts: Adding validations to verify if the area is negative.

--- src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -1,3 +1,9 @@
-export default function paintRequiredCalculator(area: number, coveragePerLiter: number): number {
+export default function paintRequiredCalculator(
+  area: number,
+  coveragePerLiter: number
+) {
+    if (area < 0){
+      throw new Error("Area should be positive");
+    }
   return area / coveragePerLiter;
 }
