cec1f1e4f3d3ef06b87303c9ff93ec4f908b564a
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-12 15:08:22 -0400

    paintRequiredCalculator.test.ts: Adding unit test when coveragePerLiter is zero.

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -15,6 +15,9 @@ describe("paint required calculator", () => {
     expect(() => paintRequiredCalculator(50, -10)).toThrow("CoveragePerLiter should be positive");
   });
 
-  test.todo("should throw an error message when the coveragePerLiter is zero.");
+  test("should throw an error message when the coveragePerLiter is zero. Dividing by zero is not possible", () => {
+    expect(() => paintRequiredCalculator(50, 0)).toThrow("CoveragePerLiter should be greater than zero");
+  });
+
   test.todo("should return expected result when the area is zero.");
 });
