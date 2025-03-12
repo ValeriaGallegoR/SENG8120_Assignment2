ed47ed5544b0b3a31310a2b7406cfe0b2c2c2622
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-12 14:16:18 -0400

    calculateCanvasSize.test.ts: Adding unit test for negative length.

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -1,9 +1,11 @@
 import calculateCanvasSize from "./calculateCanvasSize";
 
 describe("calculateCanvasSize", () => {
-  test("returns correct result", () => {
+  test("should returns correct result", () => {
     const result = calculateCanvasSize("10", "100");
 
     expect(result).toEqual(1000);
-  })
+  });
+
+  test.todo("should throw an error message when the length is negative.");
 });
